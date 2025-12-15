db09483a119fc757aaf92002d422ad913a3196d0
Satvik Mishra <satvikmishra77@gmail.com>
2025-12-15 18:24:17 +0530

    Q2. Find Customer Referee

---
+++ Q2_SQL.sql
@@ -0,0 +1,64 @@
+-- Table: Customer
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- | referee_id  | int     |
+-- +-------------+---------+
+-- In SQL, id is the primary key column for this table.
+-- Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+ 
+
+-- Find the names of the customer that are either:
+
+-- referred by any customer with id != 2.
+-- not referred by any customer.
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Customer table:
+-- +----+------+------------+
+-- | id | name | referee_id |
+-- +----+------+------------+
+-- | 1  | Will | null       |
+-- | 2  | Jane | null       |
+-- | 3  | Alex | 2          |
+-- | 4  | Bill | null       |
+-- | 5  | Zack | 1          |
+-- | 6  | Mark | 2          |
+-- +----+------+------------+
+-- Output: 
+-- +------+
+-- | name |
+-- +------+
+-- | Will |
+-- | Jane |
+-- | Bill |
+-- | Zack |
+-- +------+
+-- Explanation: Customers Will, Jane, Bill, and Zack are either not referred by any customer or referred by customers with id different from 2.
+CREATE TABLE Customer (
+  id INT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  referee_id INT
+);
+INSERT INTO Customer (id, name, referee_id) VALUES
+(1, 'Will', NULL),
+(2, 'Jane', NULL),
+(3, 'Alex', 2),
+(4, 'Bill', NULL),
+(5, 'Zack', 1),
+(6, 'Mark', 2);
+SELECT * FROM Customer;
+--- IGNORE ---
+Select name
+From Customer
+Where referee_id IS NULL OR referee_id != 2;
